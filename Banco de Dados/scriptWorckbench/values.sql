c2d57502f5850e84c75eaa0d6c83a3f847204d4e
paulaandrezza <paulaandrezza25@gmail.com>
2023-06-12 11:41:21 -0300

    add novos funcionarios que fazem aniversario esse mes

--- Banco de Dados/scriptWorckbench/values.sql
+++ Banco de Dados/scriptWorckbench/values.sql
@@ -28,8 +28,10 @@ INSERT INTO funcionario
 VALUES	(idFuncionario, 12345678900, 'João da Silva', 'Rua A, 123', 'Manhã', '111234567', 'joao.silva@example.com', '1985-07-15', 1),
 		(idFuncionario, '98765432100', 'Maria Santos', 'Avenida B, 456', 'Tarde', '2298765432', 'maria.santos@example.com', '1992-11-28', 2),
 		(idFuncionario, '55544433300', 'Pedro Oliveira', 'Rua C, 789', 'Noite', '3355554444', 'pedro.oliveira@example.com', '1979-04-03', 3),
-		(idFuncionario, '88899977700', 'Ana Pereira', 'Avenida D, 987', 'Manhã', '4488889999', 'ana.pereira@example.com', '2001-09-09', 3),
-		(idFuncionario, '22233355500', 'Carlos Santos', 'Rua E, 321', 'Tarde', '5522223333', 'carlos.santos@example.com', '1998-12-22', 3);
+		(idFuncionario, '88899977700', 'Ana Pereira', 'Avenida D, 987', 'Manhã', '4488889999', 'ana.pereira@example.com', '2001-06-09', 3),
+		(idFuncionario, '22233355500', 'Carlos Santos', 'Rua E, 321', 'Tarde', '5522223333', 'carlos.santos@example.com', '1998-12-22', 3),
+        (idFuncionario,'11122233344', 'Maria Silva', 'Avenida X, 123', 'Manhã', '5511122233', 'maria.silva@example.com', '1990-05-10', 2),
+        (idFuncionario, '33344455566', 'Ana Oliveira', 'Rua Y, 789', 'Noite', '5533344455', 'ana.oliveira@example.com', '1995-06-15', 1);
         
 
 -- -----------------------------------------------------
